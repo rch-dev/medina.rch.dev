81e5db9fc00f380ab170f9b241221a8c96922d95
six <six@rch.dev>
2019-03-14 20:28:34 -0400

    basic handshake with api

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
---
+++ src/views/Medina.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="about">
+    <h1>Test {{name}} protocol here.</h1>
+    <h1>{{handshake}}</h1>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import VueAxios from "vue-axios";
+import axios from 'axios';
+
+@Component
+export default class Medina extends Vue {
+    @Prop() name!: string;
+    @Prop() handshake!: string;
+
+    created() {
+        this.name = "medina";
+
+        axios.get('http://localhost:1001/')
+        .then(res => {
+            console.log(res)
+            this.handshake = res.data
+        })
+        .catch(e => {
+            console.log(e)
+        })
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
